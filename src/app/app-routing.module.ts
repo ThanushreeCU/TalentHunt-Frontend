5ad28ef4952de93ef9e11549cad98ba10bbf9b84
thanu <thanuuthaiah0204@gmail.com>
2021-05-23 18:03:10 +0530

    secone commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'talent-hunt',
+    loadChildren : 'src/app/talent-hunt/talent-hunt.module#TalentHuntModule'
+  },
+  {
+    path : '**',
+    redirectTo : 'talent-hunt'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
